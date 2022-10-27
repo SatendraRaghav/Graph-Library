b3603b54010059d2aee007992079b3dc5cc360f2
SatendraRaghav <Raghavji2811@gmail.com>
2022-10-27 12:15:07 +0530

    first commit

---
+++ dist/index.cjs.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var React = require('react');
+
+function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
+
+var HelloWorld = function HelloWorld() {
+  return /*#__PURE__*/React__default["default"].createElement("div", null, /*#__PURE__*/React__default["default"].createElement("h1", null, "HelloWorld this is indian large community"), /*#__PURE__*/React__default["default"].createElement("h1", null, "HelloWorld this is indian large community"), /*#__PURE__*/React__default["default"].createElement("h1", null, "HelloWorld this is indian large community"), /*#__PURE__*/React__default["default"].createElement("h1", null, "HelloWorld this is indian large community"), /*#__PURE__*/React__default["default"].createElement("h1", null, "HelloWorld this is indian large community"), /*#__PURE__*/React__default["default"].createElement("h1", null, "HelloWorld this is indian large community"));
+};
+
+exports.HelloWorld = HelloWorld;
---
+++ dist/index.esm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+var HelloWorld = function HelloWorld() {
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "HelloWorld this is indian large community"), /*#__PURE__*/React.createElement("h1", null, "HelloWorld this is indian large community"), /*#__PURE__*/React.createElement("h1", null, "HelloWorld this is indian large community"), /*#__PURE__*/React.createElement("h1", null, "HelloWorld this is indian large community"), /*#__PURE__*/React.createElement("h1", null, "HelloWorld this is indian large community"), /*#__PURE__*/React.createElement("h1", null, "HelloWorld this is indian large community"));
+};
+
+export { HelloWorld };
